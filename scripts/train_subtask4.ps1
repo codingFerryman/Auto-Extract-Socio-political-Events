a73cfa68f194ee715ab9ec413a29c121064853d8
He Liu <heliu@heliu.name>
2021-08-03 11:43:04 +0100

    correct powershell scripts

--- scripts/train_subtask4.ps1
+++ scripts/train_subtask4.ps1
@@ -1,7 +1,7 @@
 $REPO_PATH=(git rev-parse --show-toplevel)
 
-train_file=(JOIN-PATH $REPO_PATH "data/subtask4-token/en-train.json")
-test_file=$train_file
-model="roberta-base"
-dest=(JOIN-PATH $REPO_PATH "models/roberta-subtask4")
-python transformer_models.py --train_file  $train_file --test_file $test_file --dest $dest --model $model
+$train_file=(JOIN-PATH $REPO_PATH "data/subtask4-token/en-train.txt")
+$test_file=$train_file
+$model="roberta-base"
+$dest=(JOIN-PATH $REPO_PATH "models/roberta-subtask4")
+python (JOIN-PATH $REPO_PATH "src/subtask_4/transformer_models_BIO.py") --train_file  $train_file --test_file $test_file --dest $dest --model $model
