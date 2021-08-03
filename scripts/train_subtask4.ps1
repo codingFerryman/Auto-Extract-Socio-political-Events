5618fc41eb79d58bb2da1ace8909afd2d7fdfd9c
He Liu <heliu@heliu.name>
2021-08-03 12:19:21 +0100

    update running script for subtask 4

--- scripts/train_subtask4.ps1
+++ scripts/train_subtask4.ps1
@@ -1,7 +1,10 @@
+$env:CUDA_VISIBLE_DEVICES="1"
+
 $REPO_PATH=(git rev-parse --show-toplevel)
 
 $train_file=(JOIN-PATH $REPO_PATH "data/subtask4-token/en-train.txt")
 $test_file=$train_file
-$model="roberta-base"
-$dest=(JOIN-PATH $REPO_PATH "models/roberta-subtask4")
-python (JOIN-PATH $REPO_PATH "src/subtask_4/transformer_models_BIO.py") --train_file  $train_file --test_file $test_file --dest $dest --model $model
+$model="google/bigbird-roberta-base"
+$dest=(JOIN-PATH $REPO_PATH "models/bigbird-subtask4")
+$gradient_accumulation_steps=4
+python (JOIN-PATH $REPO_PATH "src/subtask_4/transformer_models_BIO.py") --train_file  $train_file --test_file $test_file --dest $dest --model $model --gradient_accumulation_steps $gradient_accumulation_steps
